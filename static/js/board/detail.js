8dfb4d51ef0f2c4b43235dbfe8154c8e7988c190
heart44 <gutt88656@gmail.com>
2022-06-24 09:34:44 +0900

    phpMVC 자습 (06.23)

--- static/js/board/detail.js
+++ static/js/board/detail.js
@@ -1,8 +1,22 @@
-(function() {   
+(function() {  
+    //글삭제 
     const btnDel = document.querySelector('#btnDel');
-    btnDel.addEventListener('click', () => {
-        if(confirm('삭제하시겠습니까?')) {
-            location.href = `del?i_board=${btnDel.dataset.i_board}`;
-        }
+    if(btnDel) {
+
+        btnDel.addEventListener('click', () => {
+            if(confirm('삭제하시겠습니까?')) {
+                location.href = `del?i_board=${btnDel.dataset.i_board}`;
+            }
+        });
+    }
+
+    //댓글삭제
+    const delBtn = document.querySelectorAll('#delRe');
+    delBtn.forEach(element => {
+        element.addEventListener('click', () => {
+            if(confirm("삭제하시겠습니까?")) {
+                location.href = `reDel?i_board=${element.dataset.i_board}&i_re=${element.dataset.i_re}`;
+            }
+        });
     });
 })();
